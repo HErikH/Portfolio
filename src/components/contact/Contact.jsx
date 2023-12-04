85586fa7bf9fbb1651074f5e2495f07c9c2a1bf7
HerikH <erik.harutyunyan.03.03@mail.ru>
2023-12-04 20:32:22 +0400

    update path

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,10 +5,10 @@ import "./style.scss";
 
 const variants = {
   initial: {
-    y: '300px',
+    y: "300px",
     opacity: 0,
   },
-  animate: { 
+  animate: {
     y: 0,
     opacity: 1,
     transition: {
@@ -29,14 +29,16 @@ function Contact() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
+    emailjs
+      .sendForm(
         "service_tk6xbyv",
         "template_a13giai",
         formRef.current,
         "GYdRzJ4Oe-eU8gbsb"
-      ).then(
+      )
+      .then(
         (result) => {
-          setSuccess(true)
+          setSuccess(true);
         },
         (error) => {
           setError(true);
@@ -67,7 +69,9 @@ function Contact() {
           <span>+37455091859</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
-          <a className="cv-download" href="/public/CV.pdf" download>Download CV</a>
+          <a className="cv-download" href="/CV.pdf" download>
+            Download CV
+          </a>
         </motion.div>
       </motion.div>
       <div className="formContainer">
@@ -79,12 +83,12 @@ function Contact() {
         >
           <svg viewBox="0 0 32.666 32.666">
             <motion.path
-            strokeWidth={0.2}
-            fill="none"
-            initial={{ pathLength: 0 }}
-            animate={isInView && { pathLength: 1 }}
-            transition={{ duration: 3 }}
-            d="M28.189,16.504h-1.666c0-5.437-4.422-9.858-9.856-9.858l-0.001-1.664C23.021,4.979,28.189,10.149,28.189,16.504z
+              strokeWidth={0.2}
+              fill="none"
+              initial={{ pathLength: 0 }}
+              animate={isInView && { pathLength: 1 }}
+              transition={{ duration: 3 }}
+              d="M28.189,16.504h-1.666c0-5.437-4.422-9.858-9.856-9.858l-0.001-1.664C23.021,4.979,28.189,10.149,28.189,16.504z
             M16.666,7.856L16.665,9.52c3.853,0,6.983,3.133,6.981,6.983l1.666-0.001C25.312,11.735,21.436,7.856,16.666,7.856z M16.333,0
             C7.326,0,0,7.326,0,16.334c0,9.006,7.326,16.332,16.333,16.332c0.557,0,1.007-0.45,1.007-1.006c0-0.559-0.45-1.01-1.007-1.01
             c-7.896,0-14.318-6.424-14.318-14.316c0-7.896,6.422-14.319,14.318-14.319c7.896,0,14.317,6.424,14.317,14.319
@@ -107,9 +111,9 @@ function Contact() {
           whileInView={{ opacity: 1 }}
           transition={{ delay: 4, duration: 1 }}
         >
-          <input type="text" required placeholder="Name" name="name"/>
-          <input type="email" required placeholder="Email" name="email"/>
-          <textarea rows={8} placeholder="Message" name="message"/>
+          <input type="text" required placeholder="Name" name="name" />
+          <input type="email" required placeholder="Email" name="email" />
+          <textarea rows={8} placeholder="Message" name="message" />
           <button>Submit</button>
           {error && "Error"}
           {success && "Success"}
@@ -117,6 +121,6 @@ function Contact() {
       </div>
     </motion.div>
   );
-};
+}
 
 export default Contact;
--- src/data/data.js
+++ src/data/data.js
@@ -1,63 +1,63 @@
 export const portfolioData = [
-    {
-      id: 1,
-      title: "React Tour",
-      img: "/public/tour.png",
-      url: "https://magenta-bublanina-57fb67.netlify.app"
-    },
-    {
-      id: 2,
-      title: "React Fitness Exercises",
-      img: "/public/exercises.png",
-      url: "https://transcendent-pasca-da4816.netlify.app"
-    },
-    {
-      id: 3,
-      title: "React Restaurant",
-      img: "/public/restaurant.png",
-      url: "https://chic-shortbread-a2921b.netlify.app"
-    },
-    {
-      id: 4,
-      title: "React Fitness",
-      img: "/public/fitness.png",
-      url: "https://herikh.github.io/FitnessHeroes/"
-    },
-    {
-      id: 5,
-      title: "React Explore Movies",
-      img: "/public/movies.png",
-      url: "https://courageous-mousse-728b22.netlify.app"
-    },
-    {
-      id: 6,
-      title: "Time Table",
-      text: "(This is a private web page !) The main purpose is to help the Institution create a timetable, if you have a wish to explore this web page contact me",
-      img: "/public/timtable.png",
-      // url: "https://papaya-bombolone-370bd1.netlify.app/"
-    },
-  ];
+  {
+    id: 1,
+    title: "React Tour",
+    img: "/tour.png",
+    url: "https://magenta-bublanina-57fb67.netlify.app",
+  },
+  {
+    id: 2,
+    title: "React Fitness Exercises",
+    img: "/exercises.png",
+    url: "https://transcendent-pasca-da4816.netlify.app",
+  },
+  {
+    id: 3,
+    title: "React Restaurant",
+    img: "/restaurant.png",
+    url: "https://chic-shortbread-a2921b.netlify.app",
+  },
+  {
+    id: 4,
+    title: "React Fitness",
+    img: "/fitness.png",
+    url: "https://herikh.github.io/FitnessHeroes/",
+  },
+  {
+    id: 5,
+    title: "React Explore Movies",
+    img: "/movies.png",
+    url: "https://courageous-mousse-728b22.netlify.app",
+  },
+  {
+    id: 6,
+    title: "Time Table",
+    text: "(This is a private web page !) The main purpose is to help the Institution create a timetable, if you have a wish to explore this web page contact me",
+    img: "/timtable.png",
+    // url: "https://papaya-bombolone-370bd1.netlify.app/"
+  },
+];
 
 export const skillsIcons = [
-  '/public/html.png',
-  '/public/css.png',
-  '/public/sass-logo.png',
-  'BEM',
-  '/public/bootstrap-logo.png',
-  '/public/js.png',
-  '/public/react.png',
-  '/public/redux.png',
-  '/public/mui.png',
-  '/public/styled-logo.png',
-  '/public/jquery-logo.png',
-  '/public/node-js.png',
-  '/public/express.png',
-  '/public/mongoose-logo.png',
-  '/public/mongodb.png',
-  '/public/github-logo.png'
-]
+  "/html.png",
+  "/css.png",
+  "/sass-logo.png",
+  "BEM",
+  "/bootstrap-logo.png",
+  "/js.png",
+  "/react.png",
+  "/redux.png",
+  "/mui.png",
+  "/styled-logo.png",
+  "/jquery-logo.png",
+  "/node-js.png",
+  "/express.png",
+  "/mongoose-logo.png",
+  "/mongodb.png",
+  "/github-logo.png",
+];
 
-// About me 
+// About me
 
 //   With my strong background in logical thinking and strategic planning, I excel at creating adaptive and cross-browser layouts for websites. ✨
 
