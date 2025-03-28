486fbb64aaa20674c4e51fd6e8cd2cd594eeadae
HErikH <erikharutyunyan398@gmail.com>
2025-03-28 18:25:38 +0400

    fix: fixed height for portfolio projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from './components/navbar/Navbar'
-import Hero from './components/hero/Hero'
-import Parallax from './components/parallax/Parallax'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Cursor from './components/cursor/Cursor'
-import About from './components/About/About'
-import './App.scss'
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
+import About from "./components/About/About";
+import "./App.scss";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Hero />
       </section>
       <section id="Portfolio">
-        <Parallax type='portfolio' />
+        <Parallax type="portfolio" />
       </section>
       <Portfolio />
       <section id="Contact">
@@ -26,7 +26,7 @@ function App() {
         <About />
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
